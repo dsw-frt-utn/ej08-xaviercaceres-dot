095820ad7eeaa3e95add265303169164e7a7b33c
Xavier Caceres <xaviercaceres03@gmail.com>
2026-05-01 17:12:06 -0300

    Problema 3 resuelto

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Product
+    {
+        private string description;
+
+        public string GetDescription() => description;
+
+        public void SetDescription(string description)
+        {
+            this.description = description;
+        }
+    }
+    internal class Problema3
+    {
+        public string CompararCopia(int originalValue, Product product)
+        {
+            int copiaOriginal = originalValue;
+            copiaOriginal++;
+
+            Product productCopia = product;
+
+            productCopia.SetDescription("Nueva Description");
+
+            return $"{originalValue}-{productCopia}-{product.GetDescription()}";
+        }
+    }
+}
