bc19ac4971af205e4511d53087a968912b6a3c7b
Xavier Caceres <xaviercaceres03@gmail.com>
2026-05-02 11:18:38 -0300

    Problema 5 resuelto

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema5
+    {
+        public decimal ObtenerImporteTotal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+
+    internal class Sale
+    {
+        protected decimal importe;  
+        public virtual decimal CalculateTotal()
+        {
+            return importe;
+        }
+        
+    }
+
+    internal class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return importe;
+        }
+    }
+
+    internal class WholesaleSale : Sale
+    {
+        protected decimal descuento;
+        public override decimal CalculateTotal()
+        {
+            descuento = importe * 0.1m;
+            return importe - descuento;
+        }
+    }
+}
